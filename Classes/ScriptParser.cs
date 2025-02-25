759e46bc8544a64ada47169d9535ec6f3592073e
chunchunmaru0000 <cho227675m@gmail.com>
2025-02-25 10:26:41 +0300

    begin GameScript ParseGameLine(string gameLine)

--- Classes/ScriptParser.cs
+++ Classes/ScriptParser.cs
@@ -2,27 +2,81 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using vlc_works.Classes;
+using System.IO;
 
 namespace vlc_works
 {
     public class ScriptParser
     {
         private string ScriptFilePath { get; set; }
+        private Encoding Encoding { get; } = Encoding.UTF8;
+        #region ERR_MSGS
+        private const string firstGameWasnFound = "В СКРИПТЕ ИГРЫ НЕ БЫЛА НАЙДЕНА ПЕРВАЯ ИГРА";
+        #endregion ERR_MSGS
 
         public ScriptParser(string scriptFilePath)
         {
-            ScriptFilePath = scriptFilePath;
+            ScriptFilePath = scriptFilePath.HebrewTrim().Trim();
         }
 
-        public List<GameScript> Parse()
+        /// <summary>
+        /// Throws an Exception if any issues with file so there is a need to use try catch
+        /// </summary>
+        /// <returns></returns>
+        public Tuple<GameScript, List<GameScript>> Parse()
         {
-            List<GameScript> gameScripts = new List<GameScript>();
+            string[] scriptLines = ParseFile();
+            string firstGameLine = FindFirstGameLine(scriptLines);
+            scriptLines = GetScriptLinesWithoutFirstGameLine(scriptLines);
 
+            GameScript firstGameScript = ParseGameLine(firstGameLine);
+            List<GameScript> gameScripts = 
+                scriptLines
+                .Select(ParseGameLine)
+                .ToList();
 
+            return new Tuple<GameScript, List<GameScript>>(firstGameScript, gameScripts);
+        }
 
-            return gameScripts;
+        private string[] ParseFile() =>
+            File
+            .ReadAllText(ScriptFilePath, Encoding)
+            .HebrewTrim()
+            .Replace("\r", "")
+            .Split('\n')
+            .Where(line => 
+                !line.StartsWith("\\"))
+            .Select(line => 
+                line
+                .Trim()
+                .ToLower())
+            .ToArray();
+
+        private string FindFirstGameLine(string[] scriptLines)
+        {
+            string firstGameLine =
+                scriptLines
+                .FirstOrDefault(line => line.StartsWith("первая"));
+
+            if (firstGameLine == null) // default
+                throw new Exception(firstGameWasnFound);
+
+            return
+                firstGameLine
+                .Split('=')
+                [1]
+                .Trim();
+        }
+
+        private string[] GetScriptLinesWithoutFirstGameLine(string[] scriptLines) =>
+            scriptLines
+                .Where(line =>
+                    !line.Contains("первая"))
+                .ToArray();
+
+        private GameScript ParseGameLine(string gameLine)
+        {
+
         }
     }
 }
