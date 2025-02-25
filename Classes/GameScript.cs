7a6a0300decdbb4885c9ccb36feb5b16d4a204e0
chunchunmaru0000 <cho227675m@gmail.com>
2025-02-25 10:04:00 +0300

    GameScript.cs and begin ScriptParser.cs

---
+++ Classes/GameScript.cs
@@ -0,0 +1,21 @@
+﻿namespace vlc_works.Classes
+{
+    public class GameScript
+    {
+        public GameType GameType { get; set; }
+        public long Lvl { get; set; }
+        public long Prize { get; set; }
+        public long Price { get; set; }
+
+        public GameScript(GameType gameType, long lvl, long prize, long price)
+        {
+            GameType = gameType;
+            Lvl = lvl;
+            Prize = prize;
+            Price = price;
+        }
+
+        public override string ToString() =>
+            $"{GameType.View()} {Lvl}; {Prize}; {Price}";
+    }
+}
---
+++ Classes/ScriptParser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vlc_works.Classes;
+
+namespace vlc_works
+{
+    public class ScriptParser
+    {
+        private string ScriptFilePath { get; set; }
+
+        public ScriptParser(string scriptFilePath)
+        {
+            ScriptFilePath = scriptFilePath;
+        }
+
+        public List<GameScript> Parse()
+        {
+            List<GameScript> gameScripts = new List<GameScript>();
+
+
+
+            return gameScripts;
+        }
+    }
+}
